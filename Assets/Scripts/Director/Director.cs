c6c1008db121aad8218f94599d2fc319894bdaa8
Zetto <usainfection@gmail.com>
2016-04-09 10:50:06 +0900

    Directorとゲームシーン用のシーンファイルを用意

---
+++ Assets/Scripts/Director/Director.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Director : MonoBehaviour {
+
+    void Awake()
+    {
+        var sceneManager = SceneManager.Instance;
+
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+    }
+}

900859a2f8a7ebc7455ba08f7aa2bfba00a1d343
Zetto <usainfection@gmail.com>
2016-04-14 10:32:16 +0900

    ゲームのシーケンス管理ベースを制作。実機動作確認済

---
+++ Assets/Scripts/State/GameState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class GameState {
+
+    public delegate void ExecuteState();
+    public ExecuteState execDelegate;
+
+    public virtual void Execute()
+    {
+        if(execDelegate != null)
+        {
+            execDelegate();
+        }
+    }
+}
---
+++ Assets/Scripts/State/GameStateProcessor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateProcessor {
+
+    private GameState m_state;
+    public GameState State
+    {
+        get { return m_state; }
+        set { m_state = value; }
+    }
+
+    public void Execute()
+    {
+        State.Execute();
+    }
+}
